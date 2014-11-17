9de37b7d31d3b811decc4a56e6acc4953ce58abd
Maria Paula Mancipe <mp.mancipe10@uniandes.edu.co>
2014-11-17 01:10:27 -0500

    Nuevo Proyecto Generado

--- ClinicaDeProblemas.web/target/ClinicaDeProblemas.web-0.0.1-SNAPSHOT/src/component/recursoMasterComponent.js
+++
@@ -1,9 +0,0 @@
-define(['component/_recursoMasterComponent'],function(_RecursoMasterComponent) {
-    App.Component.RecursoMasterComponent = _RecursoMasterComponent.extend({
-		postInit: function(){
-			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
-		}
-    });
-
-    return App.Component.RecursoMasterComponent;
-});
--- ClinicaDeProblemas.web/target/ClinicaDeProblemas.web-0.0.1-SNAPSHOT/src/component/uTIMasterComponent.js
+++
@@ -1,9 +0,0 @@
-define(['component/_uTIMasterComponent'],function(_UTIMasterComponent) {
-    App.Component.UTIMasterComponent = _UTIMasterComponent.extend({
-		postInit: function(){
-			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
-		}
-    });
-
-    return App.Component.UTIMasterComponent;
-});
