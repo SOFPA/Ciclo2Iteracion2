43d40faa2db2859595c80fb39bbcf9b6da02f00c
arvok <s.baquero10@uniandes.edu.co>
2014-11-24 23:43:57 -0500

    Registrar Usuario en UTI.
    Terminado, falla la aplicación sin razón aparente.

--- ClinicaDeProblemas.web/src/main/webapp/src/component/uTIMasterComponent.js
+++ ClinicaDeProblemas.web/src/main/webapp/src/component/uTIMasterComponent.js
@@ -1,8 +1,29 @@
 define(['component/_uTIMasterComponent'],function(_UTIMasterComponent) {
     App.Component.UTIMasterComponent = _UTIMasterComponent.extend({
-		postInit: function(){
-			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
-		}
+	postInit: function(){
+            if(usuarioActual.role.equals('Esudiante')){
+                this.registrarUsuario();
+            }
+        },
+        registrarUsuario: function(){
+            this.toolbarComponent.hideButton('create');
+            this.toolbarComponent.hideButton('save');
+            this.toolbarComponent.hideButton('print');
+            this.toolbarComponent.showButton('refresh');
+            this.toolbarComponent.hideButton('search');
+            this.toolbarComponent.hideButton('cancel');
+            this.toolbarComponent.render();
+            this.showDelete = false;
+            this.showEdit = false;
+            this.addRecordAction({
+                name: 'registrarse',
+                icon: '',
+                displayName: 'Registrarse',
+                show: true
+            },
+            this.save(),
+            usuarioActual);
+        }
     });
 
     return App.Component.UTIMasterComponent;
