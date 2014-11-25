f4a8fd68523c87fef8868df5aa6d673ec1dea0c8
arvok <s.baquero10@uniandes.edu.co>
2014-11-24 22:43:45 -0500

    primercommit
    primer commit

--- ClinicaDeProblemas.web/src/main/webapp/src/component/uTIMasterComponent.js
+++ ClinicaDeProblemas.web/src/main/webapp/src/component/uTIMasterComponent.js
@@ -1,8 +1,35 @@
 define(['component/_uTIMasterComponent'],function(_UTIMasterComponent) {
     App.Component.UTIMasterComponent = _UTIMasterComponent.extend({
-		postInit: function(){
-			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
-		}
+	postInit: function(){
+            usuarioActual: this.getLoggedUser();
+            if(usuarioActual.admin){
+                
+            }else{
+                this.registrarUsuario();
+            }
+        },
+        registrarUsuario: function(){
+            this.toolbarComponent.hideButton('create');
+            this.toolbarComponent.hideButton('save');
+            this.toolbarComponent.hideButton('print');
+            this.toolbarComponent.showButton('refresh');
+            this.toolbarComponent.hideButton('search');
+            this.toolbarComponent.hideButton('cancel');
+            this.toolbarComponent.render();
+            this.showDelete = false;
+            this.showEdit = false;
+            this.addRecordAction({
+                name: 'registrarse',
+                icon: '',
+                displayName: 'Registrarse',
+                show: true
+            },
+            this.edit,
+            this);
+        },
+        getLoggedUser: function(){
+            
+        }
     });
 
     return App.Component.UTIMasterComponent;
