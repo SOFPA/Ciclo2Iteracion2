022e3888ecd1a81a9ab4706ed82a01fd41cc77ee
SOFPA <sofpauniandes@gmail.com>
2014-11-30 21:05:07 -0500

    Test de los Test

---
+++ ClinicaDeProblemas.web/src/test/java/co/edu/uniandes/csw/SOFPA/recurso/service/RecursoServiceTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.csw.SOFPA.recurso.service;
+
+import co.edu.uniandes.csw.SOFPA.recurso.logic.dto.RecursoDTO;
+import co.edu.uniandes.csw.SOFPA.recurso.logic.dto.RecursoPageDTO;
+import javax.ejb.embeddable.EJBContainer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author estudiante
+ */
+public class RecursoServiceTest {
+    
+    public RecursoServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of createRecurso method, of class RecursoService.
+     */
+    @Test
+    public void testCreateRecurso() throws Exception {
+        System.out.println("createRecurso");
+        assertEquals(true, true);
+
+    }
+
+    /**
+     * Test of deleteRecurso method, of class RecursoService.
+     */
+    @Test
+    public void testDeleteRecurso() throws Exception {
+        System.out.println("deleteRecurso");
+        assertEquals(true, true);
+    }
+
+    /**
+     * Test of getRecurso method, of class RecursoService.
+     */
+    @Test
+    public void testGetRecurso() throws Exception {
+        System.out.println("getRecurso");
+        assertEquals(true, true);
+    }
+
+    /**
+     * Test of updateRecurso method, of class RecursoService.
+     */
+    @Test
+    public void testUpdateRecurso() throws Exception {
+        System.out.println("updateRecurso");
+        assertEquals(true, true);
+    }
+
+    /**
+     * Test of getRecursos method, of class RecursoService.
+     */
+    @Test
+    public void testGetRecursos() throws Exception {
+        System.out.println("getRecursos");
+        assertEquals(true, true);
+    }
+    
+}
